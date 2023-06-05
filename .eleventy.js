054bddecc4e46926270fc9453ba032fc20fa41a6
marleyrae <marleyrae@protonmail.com>
2023-06-04 18:53:07 -0700

    :truck: chore: Shuffling files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
-const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
-
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(EleventyVitePlugin);
+    eleventyConfig.addPlugin(require('eleventy-sass'), {
+        postcss: require('postcss')([
+            require('postcss-preset-env'),
+            require('cssnano')
+        ])
+    });
+
+    return { dir: { input: 'src', output: 'dist' } };
 }
