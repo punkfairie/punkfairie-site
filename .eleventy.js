93817679e5ec148f57f7f089b1152c3b0530dcc8
marleyrae <marleyrae@protonmail.com>
2023-06-05 18:33:36 -0700

    :hammer: chore: Add JS minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
-module.exports = function(eleventyConfig) {
+const {minify} = require('terser');
+
+module.exports = function (eleventyConfig) {
+    // Process SCSS
     eleventyConfig.addPlugin(require('eleventy-sass'), {
         postcss: require('postcss')([
             require('postcss-preset-env'),
@@ -6,5 +9,16 @@ module.exports = function(eleventyConfig) {
         ])
     });
 
-    return { dir: { input: 'src', output: 'dist' } };
+    // Minify and inline JS
+    eleventyConfig.addLiquidFilter('jsMin', async function (code, callback) {
+        try {
+            const minified = await minify(code);
+            callback(null, minified.code);
+        } catch (e) {
+            console.error(`Terser error: ${e}`);
+            callback(null, code);
+        }
+    });
+
+    return {dir: {input: 'src', output: 'dist'}};
 }
