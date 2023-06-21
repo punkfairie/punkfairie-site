1be374c912f37fbcb8f3def46d032b842f565217
marleyrae <marleyrae@protonmail.com>
2023-06-21 12:13:56 -0700

    :sparkles: feat: Add noreferrer plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,59 +1,64 @@
 const esbuild = require('esbuild')
 
 module.exports = function (eleventyConfig) {
-    // Process SCSS
-    eleventyConfig.addPlugin(require('eleventy-sass'), {
-        postcss: require('postcss')([
-            require('postcss-preset-env'),
-            require('cssnano'),
-        ]),
-    })
+  // Process SCSS
+  eleventyConfig.addPlugin(require('eleventy-sass'), {
+    postcss: require('postcss')([
+      require('postcss-preset-env'),
+      require('cssnano'),
+    ]),
+  })
 
-    // Copy CSS assets
-    eleventyConfig.addPassthroughCopy('src/style/assets')
+  // Add noopener to links
+  eleventyConfig.addPlugin(require('eleventy-plugin-automatic-noopener'), {
+    noreferrer: true,
+  })
 
-    // Copy collections & links
-    eleventyConfig.addPassthroughCopy('src/collections/images/**/*')
-    eleventyConfig.addPassthroughCopy('src/images')
+  // Copy CSS assets
+  eleventyConfig.addPassthroughCopy('src/style/assets')
 
-    // Process JS
-    eleventyConfig.addTemplateFormats('js')
-    eleventyConfig.addExtension('js', {
-        outputFileExtension: 'js',
-        compile: async (content, path) => {
-            if (path !== './src/js/main.js') {
-                return
-            }
+  // Copy collections & links
+  eleventyConfig.addPassthroughCopy('src/collections/images/**/*')
+  eleventyConfig.addPassthroughCopy('src/images')
 
-            return async () => {
-                const output = await esbuild.build({
-                    target: 'es2020',
-                    entryPoints: [path],
-                    minify: true,
-                    bundle: true,
-                    write: false,
-                })
+  // Process JS
+  eleventyConfig.addTemplateFormats('js')
+  eleventyConfig.addExtension('js', {
+    outputFileExtension: 'js',
+    compile: async (content, path) => {
+      if (path !== './src/js/main.js') {
+        return
+      }
 
-                return output.outputFiles[0].text
-            }
-        },
-    })
+      return async () => {
+        const output = await esbuild.build({
+          target: 'es2020',
+          entryPoints: [path],
+          minify: true,
+          bundle: true,
+          write: false,
+        })
 
-    // Convert img filename to alt text
-    eleventyConfig.addFilter('toAlt', (text) => {
-        const basename = text.split('.')
-        return basename[0].replace(/-/g, ' ')
-    })
+        return output.outputFiles[0].text
+      }
+    },
+  })
 
-    // Convert diary date path to slug
-    eleventyConfig.addFilter('datePathSlugify', (path) => {
-        const folders = path.split('/')
-        const file = folders.pop().split('.')
-        return `${folders[3]}/${folders[4]}/${file[0]}`
-    })
+  // Convert img filename to alt text
+  eleventyConfig.addFilter('toAlt', (text) => {
+    const basename = text.split('.')
+    return basename[0].replace(/-/g, ' ')
+  })
 
-    // RSS
-    eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-rss'))
+  // Convert diary date path to slug
+  eleventyConfig.addFilter('datePathSlugify', (path) => {
+    const folders = path.split('/')
+    const file = folders.pop().split('.')
+    return `${folders[3]}/${folders[4]}/${file[0]}`
+  })
 
-    return {dir: {input: 'src', output: 'dist'}}
+  // RSS
+  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-rss'))
+
+  return {dir: {input: 'src', output: 'dist'}}
 }
